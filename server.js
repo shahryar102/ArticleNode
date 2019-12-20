9b7370eac65bc1ccc92d6eee02de1c4719c1b732
Shahriyar <shahryar102@yahoo.com>
2019-12-19 21:39:44 -0500

    cors

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const cors = require('cors')
+
+
 require('dotenv').config()
 
+app.use(cors())
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
